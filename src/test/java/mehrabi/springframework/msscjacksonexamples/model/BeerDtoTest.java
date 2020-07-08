00da9823fe1d1ae884bef3cdd8d0ba7118e15346
Mohammad Mehrabi <mmehrabi1990@yahoo.com>
2020-07-08 22:25:58 +0200

    added JsonProperty.

--- src/test/java/mehrabi/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/mehrabi/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -23,7 +23,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserializeDto() throws JsonProcessingException {
-        String jsonString = "{\"id\":\"973a2c55-33ea-4972-8162-a623142576e6\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-07-08T17:11:36.3071613+03:00\",\"lastUpdateDate\":\"2020-07-08T17:11:36.8170942+03:00\"}\n";
+        String jsonString = "{\"beerId\":\"973a2c55-33ea-4972-8162-a623142576e6\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-07-08T17:11:36.3071613+03:00\",\"lastUpdateDate\":\"2020-07-08T17:11:36.8170942+03:00\"}\n";
 
         BeerDto beerDto = objectMapper.readValue(jsonString,BeerDto.class);
 
